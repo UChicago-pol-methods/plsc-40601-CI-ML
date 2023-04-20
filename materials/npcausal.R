838b80a7ce5c1ae651f3757496531063d951a6ec
Molly Offer-Westort <mollyow@gmail.com>
2023-04-20 13:37:47 -0500

    npcausal

---
+++ materials/npcausal.R
@@ -0,0 +1,17 @@
+# install.packages("devtools")
+# install.packages(c('earth', 'SuperLearner', 'gam', 'ranger', 'rpart'))
+
+library(devtools)
+# install_github("ehkennedy/npcausal")
+library(earth)
+library(SuperLearner)
+library(gam)
+library(ranger)
+library(rpart)
+library(npcausal)
+
+
+n <- 1000; x <- matrix(rnorm(n*5),nrow=n)
+a <- sample(3,n,replace=TRUE); y <- rnorm(n)
+
+ate.res <- ate(y,a,x)
